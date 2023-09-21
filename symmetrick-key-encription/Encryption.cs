aa944872655b647683e5a14847808f1c51fba111
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-09-21 10:29:51 +0200

    .

---
+++ symmetrick-key-encription/Encryption.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace symmetrick_key_encription;
+
+public class Encryption
+{
+    public void Encrypt(string message, byte[] key)
+    {
+        using var aes = new AesGcm(key, AesGcm.TagByteSizes.MaxSize);
+        byte[] size32key = new byte[32];
+        Array.Copy(key, size32key, key.Length);
+        var iv = new byte[AesGcm.NonceByteSizes.MaxSize]; // MaxSize = 12
+        RandomNumberGenerator.Fill(iv);
+        
+        byte[] plaintextBytes = Encoding.Unicode.GetBytes(message);
+        byte[] encrypted = new byte[plaintextBytes.Length];
+        var tag = new byte[AesGcm.TagByteSizes.MaxSize]; // MaxSize = 16
+        
+        aes.Encrypt(iv, plaintextBytes, encrypted, tag);
+
+        Console.WriteLine($"Encrypted message: {Convert.ToBase64String(encrypted)}");
+    }
+}
---
+++ symmetrick-key-encription/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using symmetrick_key_encription;
+Encryption encryption = new Encryption();
+Console.WriteLine("Passphrase: ");
+
+var pass = string.Empty;
+ConsoleKey key;
+do
+{
+    var keyInfo = Console.ReadKey(intercept: true);
+    key = keyInfo.Key;
+
+    if (key == ConsoleKey.Backspace && pass.Length > 0)
+    {
+        Console.Write("\b \b");
+        pass = pass[0..^1];
+    }
+    else if (!char.IsControl(keyInfo.KeyChar))
+    {
+        Console.Write("*");
+        pass += keyInfo.KeyChar;
+    }
+} while (key != ConsoleKey.Enter);
+
+
+Console.WriteLine();
+Console.WriteLine("----------------------------------");
+
+
+while (true)
+{
+    Console.WriteLine("1: Safely store message\n2: Read message\n0: Exit");
+    try
+    {
+        int? option = Int32.Parse(Console.ReadLine());
+
+        if (option == 1)
+        {
+            string? message = Console.ReadLine();
+            var passKey = Encoding.UTF8.GetBytes(pass);
+            encryption.Encrypt(message, passKey);
+        }
+        else if (option == 2)
+        {
+            
+        } 
+        else if (option == 0)
+        {
+            break;
+        } 
+        else
+        {
+            throw new Exception();
+        }
+        
+    } catch (Exception e)
+    {
+        Console.WriteLine("Invalid option");
+        Console.WriteLine(e.Message);
+    } 
+    Console.WriteLine("----------------------------------");
+}
